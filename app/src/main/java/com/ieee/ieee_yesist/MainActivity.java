989275f5311f8464d7e89120585fbc406086b843
Amit Agarwal <amitagarwalchas@gmail.com>
2021-05-16 04:16:03 +0530

    Fix: Side Nav and Bottom Nav

--- app/src/main/java/com/ieee/ieee_yesist/MainActivity.java
+++ app/src/main/java/com/ieee/ieee_yesist/MainActivity.java
@@ -1,39 +1,24 @@
 package com.ieee.ieee_yesist;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
-import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentTransaction;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.fragment.NavHostFragment;
-
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.Gravity;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
-import android.widget.Toast;
-
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
-
-import com.google.android.material.bottomnavigation.BottomNavigationItemView;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.navigation.NavigationView;
 import com.ieee.ieee_yesist.databinding.ActivityMainBinding;
-import com.ieee.ieee_yesist.view.AboutTeam.AboutTeamFragment;
-import com.ieee.ieee_yesist.view.HomeFragment;
-import com.ieee.ieee_yesist.view.Timeline.TimelineFragment;
-import com.ieee.ieee_yesist.view.Tracks.TracksFragment;
 
 public class MainActivity extends AppCompatActivity {
 
     private ActivityMainBinding binding;
     private NavHostFragment navHostFragment;
+    private NavController navController;
     BottomNavigationView bottomNavigationView;
     private AppBarConfiguration mAppBarConfiguration;
     DrawerLayout drawer;
@@ -46,7 +31,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(view);
 
         bottomNavigationView = findViewById(R.id.bottomNavigationView);
-        drawer = findViewById(R.id.drawer_layout);
 
         NavigationView navigationView = findViewById(R.id.nav_view);
         mAppBarConfiguration = new AppBarConfiguration.Builder(
@@ -55,58 +39,35 @@ public class MainActivity extends AppCompatActivity {
                 .setDrawerLayout(drawer)
                 .build();
 
-        NavController navController = Navigation.findNavController(this, R.id.fragNavHost);
+        navController = Navigation.findNavController(this, R.id.fragNavHost);
         NavigationUI.setupWithNavController(bottomNavigationView, navController);
         NavigationUI.setupWithNavController(navigationView, navController);
 
-        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
-            @Override
-            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-                Fragment selFragment = null;
-                switch (item.getItemId()){
-                    case R.id.more_options:
-                        drawer.openDrawer(GravityCompat.START);
-                        return true;
-                    case R.id.trendingFragment:
-                        Toast.makeText(MainActivity.this,"Timeline start",Toast.LENGTH_SHORT).show();
-                        selFragment= new TimelineFragment();
-                        loadFragment(selFragment);
-                        return true;
-                    case R.id.homeFragment:
-                        Toast.makeText(MainActivity.this,"Home start",Toast.LENGTH_SHORT).show();
-                        selFragment= new HomeFragment();
-                        loadFragment(selFragment);
-                        return true;
-                    case R.id.tracksFragment:
-                        Toast.makeText(MainActivity.this,"Tracks start",Toast.LENGTH_SHORT).show();
-                        selFragment= new TracksFragment();
-                        loadFragment(selFragment);
-                        return true;
-                    case R.id.aboutTeamFragment:
-                        Toast.makeText(MainActivity.this,"About team start",Toast.LENGTH_SHORT).show();
-                        selFragment= new AboutTeamFragment();
-                        loadFragment(selFragment);
-                        return true;
-                    default:
-                        drawer.closeDrawer(GravityCompat.END);
-                        return true;
-                }
+        bottomNavigationView.setOnNavigationItemSelectedListener(item -> {
+            if(item.getItemId() == R.id.more_options) {
+                binding.drawerLayout.openDrawer(GravityCompat.START);
             }
+            if(binding.drawerLayout.isDrawerOpen(GravityCompat.END)) {
+                binding.drawerLayout.closeDrawer(GravityCompat.END);
+            }
+            return NavigationUI.onNavDestinationSelected(item, navController);
         });
 
-    }
+        binding.drawerLayout.setDrawerLockMode(DrawerLayout.LOCK_MODE_LOCKED_CLOSED);
 
-    private void loadFragment(Fragment selFragment) {
-        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
-        transaction.replace(R.id.fragNavHost, selFragment,null);
-        transaction.commit();
     }
-
 
     @Override
     public boolean onSupportNavigateUp() {
-        NavController navController = Navigation.findNavController(this, R.id.fragNavHost);
-        return NavigationUI.navigateUp(navController, mAppBarConfiguration)
-                || super.onSupportNavigateUp();
+        return NavigationUI.navigateUp(navController, mAppBarConfiguration);
+    }
+
+    @Override
+    public void onBackPressed() {
+        if(binding.drawerLayout.isDrawerOpen(GravityCompat.START)) {
+            binding.drawerLayout.closeDrawer(GravityCompat.START);
+        }else {
+            super.onBackPressed();
+        }
     }
 }
